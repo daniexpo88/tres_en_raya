9bb1c9a5df15b3e08cb7febcc38e2f2111616607
daniexpo88 <daniel8exposito@gmail.com>
2022-01-24 19:49:22 +0100

    Initial commit

--- EjercicioConcurrenciaSocket/src/com/ejconcsocket/grafico/partida/Principal.java
+++
@@ -1,7 +0,0 @@
-package com.ejconcsocket.grafico.partida;
-
-public class Principal {
-	public static void main(String[] args) {
-		MarcoPrincipal mimarco = new MarcoPrincipal();
-	}
-}
--- EjercicioConcurrenciaSocket/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module EjercicioConcurrenciaSocket {
-	requires java.desktop;
-}
